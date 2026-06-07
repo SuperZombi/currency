bef1b77d2db17ef02fa33704d75db5e39907af80
Super Zombi <super.zombi.yt@gmail.com>
2026-06-07 23:33:37 +0100

    Show ISO code first and dim currency symbol

--- app.jsx
+++ app.jsx
@@ -297,8 +297,8 @@ const CurrencyCard = ({
 				<i className="fa-solid fa-bars pointer-events-none"></i>
 			</button>
 			<div className="flex select-none whitespace-nowrap items-center gap-2 font-mono font-medium">
-				<span>{currency.symbol}</span>
 				<span>{currency.iso_code}</span>
+				<span className="text-gray-500">{currency.symbol}</span>
 			</div>
 			<div className="flex items-center gap-4 w-full">
 				<div className="flex items-center gap-1 w-full">
